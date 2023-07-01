e7af2579f3f68c89a00530764b24fe1aa8b246a5
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-07-01 15:26:44 +0545

    feat: Theme with dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
 const App = () => {
+  const [theme, colorMode] = useMode();
   return (
-    <div>App</div>
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className="app">
+        <main className="content"></main>
+      </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 

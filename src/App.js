8e3fab37213fb3df89170e171be531862f1e84bc
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-07-04 23:11:51 +0545

    Feat: Router Setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+import Team from "./pages/team";
+import Invoices from "./pages/invoices";
+import Contacts from "./pages/contacts";
+import Bar from "./pages/bar";
+import Form from "./pages/form";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
+import FAQ from "./pages/faq";
+import Geography from "./pages/geography";
+import Calendar from "./pages/calendar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <div className="app">
-        <main className="content">
-          <Topbar/>
-        </main>
-      </div>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar/>
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/invoice" element={<Invoices />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/fqa" element={<FAQ />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
     </ColorModeContext.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;

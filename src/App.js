198647193e49d6a059d208e2198b3bd424cd59b0
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-07-13 22:48:05 +0545

    feat: invoice bill

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
 import Team from "./pages/team";
-// import Invoices from "./pages/invoices";
+import Invoices from "./pages/invoices";
 import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
 // import Form from "./pages/form";
@@ -22,15 +22,15 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar/>
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
               {/*<Route path="/calendar" element={<Calendar />} />
-              <Route path="/invoice" element={<Invoices />} />
               <Route path="/line" element={<Line />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />

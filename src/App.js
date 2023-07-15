f47dbce8d5390517744d04f16d0631c39ad072f7
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-07-15 11:41:54 +0545

    feat: Profile form

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from "./pages/team";
 import Invoices from "./pages/invoices";
 import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
-// import Form from "./pages/form";
+import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
@@ -30,10 +30,10 @@ const App = () => {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/*<Route path="/calendar" element={<Calendar />} />
               <Route path="/line" element={<Line />} />
               <Route path="/bar" element={<Bar />} />
-              <Route path="/form" element={<Form />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/fqa" element={<FAQ />} /> */}

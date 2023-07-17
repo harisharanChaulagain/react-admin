f3f1be32669d464dc92b3fa77b5fe2916d194167
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-07-17 21:13:59 +0545

    feat: Bar Chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./pages/dashboard";
 import Team from "./pages/team";
 import Invoices from "./pages/invoices";
 import Contacts from "./pages/contacts";
-// import Bar from "./pages/bar";
+import Bar from "./pages/bar";
 import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
@@ -32,8 +32,8 @@ const App = () => {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-             {/* <Route path="/line" element={<Line />} />
               <Route path="/bar" element={<Bar />} />
+             {/* <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/geography" element={<Geography />} />*/}
               <Route path="/faq" element={<FAQ />} /> 
